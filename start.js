4b5c206b52b924ae53601c4c9c9c9f44252e300f
valoulol <valentin.fruchet@gmail.com>
2014-11-13 16:03:55 +0100

    Héhéé

--- start.js
+++ start.js
@@ -1,1 +1,9 @@
-console.log("prout prout");
+_ = require("lodash") ; // Bibliothèque permettant d'étendre les capacités du JS classique : https://lodash.com/docs
+var express = require('express') // Bilbliothèque permettant de créer le serveur web (remplace Apache) ;
+
+// Création du serveur
+var app = express() ;
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+var server = app.listen(8888) ;
