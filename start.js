f6ca3d2ecf518aa00fbb8cc05415501f609b3e7d
MaximeGoislot <goislotm@gmail.com>
2014-11-27 11:01:29 +0100

    vue

--- start.js
+++ start.js
@@ -1,10 +1,16 @@
 
 _ = require("lodash") ; // Bibliothèque permettant d'étendre les capacités du JS classique : https://lodash.com/docs
 var express = require('express') // Bilbliothèque permettant de créer le serveur web (remplace Apache) ;
+var Twig = require('twig'), // Twig module
+    twig = Twig.twig;       // Render function
+
+
 
 // Création du serveur
 var app = express() ;
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('vue.html.twig', {
+    message : "Hello World"
+  });
 })
 var server = app.listen(8888) ;
