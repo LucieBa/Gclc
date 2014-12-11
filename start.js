c95f62cab518a1eff8fe44a9e0b22e3ea40a1027
MaximeGoislot <goislotm@gmail.com>
2014-12-11 16:31:40 +0100

    bite

--- start.js
+++ start.js
@@ -3,14 +3,24 @@ _ = require("lodash") ; // Bibliothèque permettant d'étendre les capacités du
 var express = require('express') // Bilbliothèque permettant de créer le serveur web (remplace Apache) ;
 var Twig = require('twig'), // Twig module
     twig = Twig.twig;       // Render function
-
+fs = require('fs');
 
 
 // Création du serveur
 var app = express() ;
 app.get('/', function (req, res) {
+
   res.render('main_page.html.twig', {
     message : "Hello World"
   });
 })
+
+app.get('/logo', function (req, res) {
+  fs.readFile('logo.png', function (err, data) {
+  if (err) throw err;
+  res.write(data);
+});
+
+})
+
 var server = app.listen(8888) ;
