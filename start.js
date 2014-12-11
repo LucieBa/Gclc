f2996a6742bbe098ccf7bb3772dc5b1500c73857
MaximeGoislot <goislotm@gmail.com>
2014-12-11 19:55:43 +0100

    news

--- start.js
+++ start.js
@@ -2,27 +2,39 @@
 //			Requires
 /////////////////////////////////////////////////////////
 _ = require("lodash") ; // Bibliothèque permettant d'étendre les capacités du JS classique : https://lodash.com/docs
-var express = require('express') // Bilbliothèque permettant de créer le serveur web (remplace Apache) ;
+var express        =        require("express");
+var bodyParser     =        require("body-parser");
+var app            =        express();
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+
 var Twig = require('twig'), // Twig module
     twig = Twig.twig;       // Render function
 fs = require('fs');
 
 
+var news = JSON.parse('{"data": [] }');
+
+
 // Création du serveur
-var app = express() ;
 app.get('/', function (req, res) {
 
   res.render('main_page.html.twig', {
-    message : "Hello World"
+    message : news
   });
 })
 
 app.get('/logo', function (req, res) {
-  fs.readFile('logo.png', function (err, data) {
-  if (err) throw err;
-  res.write(data);
-});
+  	fs.readFile('logo.png', function (err, data) {
+  	if (err) throw err;
+  		res.write(data);
+	});
 
 })
+
+app.post('/News', function (req, res) {
+	news.data.push({ 'titre': req.body.titre, 'contenu': req.body.contenu });
+  	res.json(news);
+});
 
 var server = app.listen(8888) ;
